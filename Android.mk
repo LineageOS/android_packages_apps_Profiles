c3fcae9b0a3f0cae19d2f386c3e32b7a50f4f7b0
Bruno Martins <bgcngm@gmail.com>
2018-08-30 20:12:33 +0100

    Profiles: Build with AAPT2
    Change-Id: I99e53e59daeb952e09e4063f3dd9e77e77eaac27

--- Android.mk
+++ Android.mk
@@ -19,13 +19,18 @@ include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
-LOCAL_STATIC_JAVA_LIBRARIES := \
+LOCAL_STATIC_ANDROID_LIBRARIES := \
     android-support-v4 \
     android-support-v7-appcompat \
     android-support-v7-preference \
     android-support-v7-recyclerview \
-    android-support-v14-preference \
+    android-support-v14-preference
+
+LOCAL_STATIC_JAVA_LIBRARIES := \
     org.lineageos.platform.internal
+
+LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
+LOCAL_USE_AAPT2 := true
 
 LOCAL_MODULE_TAGS := optional
 
