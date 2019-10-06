0bcf521d6258cb5aee75e08c15fd25682fa4796c
Han Wang <416810799@qq.com>
2019-10-06 15:45:52 +0200

    Revert "Profiles: explicitly include android support libs"
    This reverts commit f84a40fb05a427d4669fc21586e13165e9b38a76.
    
    Reason for revert: <INSERT REASONING HERE>
    
    Change-Id: I35d44a71972104d4a4fd21d6cd38dac72469471d

--- Android.mk
+++ Android.mk
@@ -19,13 +19,6 @@ include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
-LOCAL_STATIC_ANDROID_LIBRARIES := \
-    android-support-v4 \
-    android-support-v7-appcompat \
-    android-support-v7-preference \
-    android-support-v7-recyclerview \
-    android-support-v14-preference
-
 LOCAL_STATIC_JAVA_LIBRARIES := \
     org.lineageos.platform.internal
 
